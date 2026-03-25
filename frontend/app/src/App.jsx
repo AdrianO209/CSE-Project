7763a582d8b23688c296d5821e0c80dcfac9c300
Adrian Ortiz Jr <adrianortizjr@icloud.com>
2026-03-25 14:21:07 -0700

    starting to work on user interface for the teachers

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -13,6 +13,56 @@ import "./App.css";
 
 function App() {
   const [isLogin, setLogin] = useState(false);
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+=======
+>>>>>>> Stashed changes
+  const [role, setRole] = useState("");
+  const [usernameAttempt, setUserNameAttempt] = useState("");
+  const [passwordAttempt, setPasswordAttempt] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+
+  const checkLogin = async () => {
+    setPasswordError("");
+    setUsernameError("");
+
+    const response = await fetch("http://127.0.0.1:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: usernameAttempt,
+        password: passwordAttempt,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      if (response.status === 404) {
+        setUsernameError(result.usernameError);
+        return;
+      } else {
+        setPasswordError(result.passwordError);
+        return;
+      }
+    }
+
+    const role = result.role;
+
+    if (role === "admin") {
+      window.location.href = "http://127.0.0.1:5000/admin/user/";
+      setLogin(true);
+    } else if (role === "student") {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  };
+>>>>>>> Stashed changes
 
   if (!isLogin) {
     return (
@@ -42,18 +92,29 @@ function App() {
         </Box>
       </ThemeProvider>
     );
+  } else if (role === "teacher") {
+    return (
+      <ThemeProvider theme={auraTheme}>
+        <CssBaseline />
+        <header className="header">
+          <Box className="menu">
+            <Typography variant="h1">Aura</Typography>
+          </Box>
+        </header>
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <ThemeProvider theme={auraTheme}>
+        <CssBaseline />
+        <header className="header">
+          <Box className="menu">
+            <Typography variant="h1">Aura</Typography>
+          </Box>
+        </header>
+      </ThemeProvider>
+    );
   }
-
-  return (
-    <ThemeProvider theme={auraTheme}>
-      <CssBaseline />
-      <header>
-        <Box className="title">
-          <Typography variant="h1">Aura</Typography>
-        </Box>
-      </header>
-    </ThemeProvider>
-  );
 }
 
 export default App;
